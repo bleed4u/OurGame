e973e5e5983656208355aa8d381d50963d5a932b
Maxim Lozhkin <maximlozhking2004@gmail.com>
2023-07-04 23:47:29 +0300

    Сделал UI игры
    Сделал pause menu, реализовал:
    resume game, restart game, exit

---
+++ ourGame/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+        ResumeGame();
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
